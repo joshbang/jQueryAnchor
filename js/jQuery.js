2140a4c39e593c49c9cd97cfec2aea86ef2e99b2
atrevisanut <atrevisanut@gmail.com>
2016-09-29 02:39:34 -0600

    updated links to smooth scroll

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,4 +1,46 @@
 $(document).ready(function() {
+  $(function() {
+      // Select link by id and add click event
+      $('#link1').click(function() {
+
+        // Animate Scroll to #bottom
+        $('html,body').animate({
+          scrollTop: $("#part-one").offset().top }, // Tell it to scroll to the top #bottom
+          2000 // How long scroll will take in milliseconds
+        );
+
+        // Prevent default behavior of link
+        return false;
+      });
+    });
+  $(function() {
+      // Select link by id and add click event
+      $('#link2').click(function() {
+
+        // Animate Scroll to #bottom
+        $('html,body').animate({
+          scrollTop: $("#part-two").offset().top }, // Tell it to scroll to the top #bottom
+          2000 // How long scroll will take in milliseconds
+        );
+
+        // Prevent default behavior of link
+        return false;
+      });
+    });
+    $(function() {
+        // Select link by id and add click event
+        $('#link3').click(function() {
+
+          // Animate Scroll to #bottom
+          $('html,body').animate({
+            scrollTop: $("#part-three").offset().top }, // Tell it to scroll to the top #bottom
+            2000 // How long scroll will take in milliseconds
+          );
+
+          // Prevent default behavior of link
+          return false;
+        });
+      });
 
   $(function() {
       // Select link by id and add click event
