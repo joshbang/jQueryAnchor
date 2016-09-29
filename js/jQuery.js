a73716821efdd687ff7dcf062fc704df6d1408ce
atrevisanut <atrevisanut@gmail.com>
2016-09-29 02:34:42 -0600

    contains answers to the code

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
 
+  $(function() {
+      // Select link by id and add click event
+      $('#anchor1').click(function() {
+
+        // Animate Scroll to #bottom
+        $('html,body').animate({
+          scrollTop: $("#part-two").offset().top }, // Tell it to scroll to the top #bottom
+          2000 // How long scroll will take in milliseconds
+        );
+
+        // Prevent default behavior of link
+        return false;
+      });
+    });
+
+    $(function() {
+        // Select link by id and add click event
+        $('#anchor2').click(function() {
+
+          // Animate Scroll to #bottom
+          $('html,body').animate({
+            scrollTop: $("#part-three").offset().top }, // Tell it to scroll to the top #bottom
+            2000 // How long scroll will take in milliseconds
+          );
+
+          // Prevent default behavior of link
+          return false;
+        });
+      });
 });
